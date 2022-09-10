778696e2c49dc5d6624cebbb902ce2cbb20576ff
Jordi Bofill <jordi.bofill@gmail.com>
2022-09-10 12:33:02 +0200

    Add list of poems and test

--- abello/poems.py
+++ abello/poems.py
@@ -1,5 +1,8 @@
-poem_1 = ['SOVINT DIEM',
-'''Sovint diem
+import random
+
+poems = [
+    ['SOVINT DIEM',
+     '''Sovint diem
 això és la fi,
 cap música ja no controla
 les nostres esperances.
@@ -11,17 +14,37 @@ que amatents escolten
 allà al fons de la nit.
 Aquesta és la força que busquem,
 l’amor que aprenem a sostenir
-contra el caire del temps''']
+contra el caire del temps'''],
+    [
+        ['''El blat del temps (1986)'''],
+        ['''Sense voler escolto
+el so de paraules
+que ja no tenen cap
+valor.
+Paraules que foren
+dites en moments
+que creguérem de gran
+lucidesa. I ara les
+trobem mortes,
+llençades vora els cancells
+de portes que no ens
+atrevim a obrir. ''']
+    ]
+]
+
 
 def get_poem():
-    title = poem_1[0]
-    poem = poem_1[1]
+    x = random.randint(0, len(poems)-1)
+    title = poems[x][0]
+    poem = poems[x][1]
     return title, poem
+
 
 def main():
     title, poem = get_poem()
     print(title)
     print(poem)
+
 
 if __name__ == "__main__":
     main()
--- tests/test_poem.py
+++ tests/test_poem.py
@@ -1,8 +1,17 @@
 from abello.poems import main
 
+
 def test_main(capsys):
     main()
     out, err = capsys.readouterr()
     assert out.count('\n') >= 2
-    
-    
+
+def test_poems():
+    from abello.poems import poems
+    assert isinstance(poems, list)
+    assert len(poems) > 0
+    for poem in poems:
+        assert isinstance(poem, list)
+        assert len(poem) == 2
+        assert len(poem[0]) > 0
+        assert len(poem[1]) > 0
